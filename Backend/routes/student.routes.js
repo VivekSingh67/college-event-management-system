d867f058f105958577cb8beac823c1f498493d79
Vivek Singh <vivekSingh2003v@gmail.com>
2026-03-11 20:19:12 +0530

    11March

--- Backend/routes/student.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const routes = express.Router()
-const authMiddleware = require('../middlewares/auth.middleware')
-const studentController = require("../controllers/student.controller")
-
-routes.get("/get-data", authMiddleware.authUserMiddleware , studentController.getData)
-routes.get("/get-data/:id", authMiddleware.authUserMiddleware , studentController.getDataById)
-
-module.exports = routes
